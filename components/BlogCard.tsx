f79944f893bf2b83d0737fbc9caa3dee0cfc129d
jiun-dev <jiun.dev@gmail.com>
2022-12-07 12:36:42 +0900

    blog list style

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -9,9 +9,34 @@ interface Props {
 const BlogCard = ({ title, description, slug }: Props) => {
   return (
     <Link href={"/blogs/" + slug}>
-      <div className="bg-green-100 p-2 rounded cursor-pointer">
-        <h1 className="text-grey-900 text-3xl font-bold">{title}</h1>
-        <p className="text-grey-500">{description}</p>
+      <div className="py-8 flex flex-wrap md:flex-nowrap">
+        <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span className="font-semibold title-font text-gray-700">
+            CATEGORY
+          </span>
+          <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+        </div>
+        <div className="md:flex-grow">
+          <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+            {title}
+          </h2>
+          <div className="leading-relaxed">{description}</div>
+          <div className="text-indigo-500 inline-flex items-center mt-4">
+            Learn More
+            <svg
+              className="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </div>
+        </div>
       </div>
     </Link>
   );
--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -22,15 +22,21 @@ type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 const Blogs: NextPage<Props> = ({ posts }) => {
   return (
-    <div className="max-w-3xl mx-auto p-5">
-      {posts.map((post) => (
-        <BlogCard
-          key={post.slug}
-          title={post.title}
-          description={post.meta}
-          slug={post.slug}
-        />
-      ))}
+    <div>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="-my-8 divide-y-2 divide-gray-100">
+            {posts.map((post) => (
+              <BlogCard
+                key={post.slug}
+                title={post.title}
+                description={post.meta}
+                slug={post.slug}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
