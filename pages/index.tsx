35b625d19f7ed5f13c5f1fc86b7df5df1a7f3938
jiun-dev <jiun.dev@gmail.com>
2022-12-09 06:44:42 +0900

    chage home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,42 @@
-import Head from "next/head";
-import Image from "next/image";
+import { InferGetStaticPropsType, NextPage } from "next";
+import BlogCard from "../components/BlogCard";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { readPostsInfo } from "../lib/helper";
+import { PostApiResponse } from "../utils/types";
 
-export default function Home() {
-  return <div></div>;
-}
+export const getStaticProps = async () => {
+  const postInfo: PostApiResponse = readPostsInfo();
+
+  return {
+    props: { posts: postInfo },
+  };
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Blogs: NextPage<Props> = ({ posts }) => {
+  return (
+    <div>
+      <Header />
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="-my-8 divide-y-2 divide-gray-100">
+            {posts.map((post) => (
+              <BlogCard
+                key={post.slug}
+                title={post.title}
+                description={post.meta}
+                slug={post.slug}
+                date={post.date}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default Blogs;
